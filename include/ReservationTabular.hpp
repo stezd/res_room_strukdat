b67678d9cec40f494c1a5781eb59c2b800969bcb
Aldi Pramudya <llenndesu@gmail.com>
2025-05-30 09:53:07 +0700

    feat: ReservationTabular.hpp is not done yet

---
+++ include/ReservationTabular.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Reservation.hpp"
+#include "BookingSystem.hpp"
+#include <vector>
+#include <iostream>
+
+class ReservationTabular {
+    std::vector<Reservation> ReservationVector;
+public:
+
+};
+
+
