e4d26c24af490f725a4ca35b68b689579810a304
Aldi Pramudya <llenndesu@gmail.com>
2025-05-30 09:52:10 +0700

    feat: BookingSystem.hpp now can pop the rsv boject and return it

--- include/BookingSystem.hpp
+++ include/BookingSystem.hpp
@@ -3,37 +3,49 @@
 #include "data_structures/Queue.hpp"
 #include "Reservation.hpp"
 #include <iostream>
+
 class BookingSystem {
     Queue<Reservation> waitingQueue;
 
 public:
     void tambahReservasi(const Reservation &rsv) {
-            waitingQueue.enqueue(rsv);
+        waitingQueue.enqueue(rsv);
+    }
+
+    void tampilkanAntrean() const {
+        if (waitingQueue.empty()) {
+            std::cout << "Antrean kosong" << std::endl;
+            return;
         }
 
-        void tampilkanAntrean() const {
-            if (waitingQueue.empty()) {
-                std::cout << "Antrean kosong" << std::endl;
-                return;
-            }
+        Queue<Reservation> tempQueue = waitingQueue;
+        int idx = 1;
+        std::cout << "\nDaftar Antrean Reservasi:\n" << std::endl;
 
-            Queue<Reservation> tempQueue = waitingQueue;
-            int idx = 1;
-            std::cout << "\nDaftar Antrean Reservasi:\n" << std::endl;
-
-            while (!tempQueue.empty()) {
-                const Reservation &rsv = tempQueue.peek();
-                std::cout << idx << ". "
-                        << "ID: " << rsv.getId()
-                        << " | Nama: " << rsv.getNama()
-                        << " | NIM: " << rsv.getNim()
-                        << " | Ruangan: " << rsv.getRuangan()
-                        << " | Tanggal: " << rsv.getTanggal()
-                        << " | Waktu: " << rsv.getJamMulai() << "-" << rsv.getJamSelesai()
-                        << " | Status: " << rsv.getStatus() << std::endl;
-                tempQueue.dequeue();
-                ++idx;
-            }
-            std::cout << std::endl;
+        while (!tempQueue.empty()) {
+            const Reservation &rsv = tempQueue.peek();
+            std::cout << idx << ". "
+                    << "ID: " << rsv.getId()
+                    << " | Nama: " << rsv.getNama()
+                    << " | NIM: " << rsv.getNim()
+                    << " | Ruangan: " << rsv.getRuangan()
+                    << " | Tanggal: " << rsv.getTanggal()
+                    << " | Waktu: " << rsv.getJamMulai() << "-" << rsv.getJamSelesai()
+                    << " | Status: " << rsv.getStatus() << std::endl;
+            tempQueue.dequeue();
+            ++idx;
         }
+
+        
+        std::cout << std::endl;
+    }
+
+    Reservation prosesReservasi() {
+        if (waitingQueue.empty()) {
+            throw std::runtime_error("Antrean kosong");
+        }
+        Reservation rsv = waitingQueue.peek();
+        waitingQueue.dequeue();
+        return rsv;
+    }
 };
