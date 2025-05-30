3459cd7a080b9827334ded683d2acf06e3cf1b21
Aldi Pramudya <llenndesu@gmail.com>
2025-05-30 19:01:34 +0700

    feat: add reservation management methods to ReservationTabular

--- include/ReservationTabular.hpp
+++ include/ReservationTabular.hpp
@@ -3,10 +3,33 @@
 #include "Reservation.hpp"
 #include "BookingSystem.hpp"
 #include <vector>
-#include <iostream>
+#include <string>
 
 class ReservationTabular {
-    std::vector<Reservation> ReservationVector;
+    std::vector<Reservation> ReservationTable;
 
 public:
+    std::optional<Reservation> get_reservation_by_id(const std::string &id) const {
+        const auto it =
+                std::ranges::find_if(ReservationTable, [&id](const Reservation &rsv) {
+                    return rsv.getId() == id;
+                });
+        if (it != ReservationTable.end()) {
+            return *it;
+        }
+        return std::nullopt;
+    }
+
+    void push(const Reservation &rsv) {
+        ReservationTable.push_back(rsv);
+    }
+
+    void erase(const std::string &id) {
+        auto it = std::ranges::find_if(ReservationTable.begin(), ReservationTable.end(),
+                                       [&id](const Reservation &rsv) { return rsv.getId() == id; });
+        if (it == ReservationTable.end()) {
+            throw std::runtime_error("Reservation with given ID does not exist");
+        }
+        ReservationTable.erase(it);
+    }
 };
